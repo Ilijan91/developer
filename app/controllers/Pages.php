c2404f95e805aa2c2177392c930fe5b13c85ab8f
unknown <ilijan.militar@gmail.com>
2019-12-30 20:27:55 +0100

    adding Database class + navigation

--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -1,24 +1,20 @@
 <?php
   class Pages extends Controller {
+
     public function __construct(){
      
     }
     
     public function index(){
+
+      $user= 
       $data = [
-        'title' => 'Home',
+        'title' => 'Welcome',
         
       ];
      
       $this->view('pages/index', $data);
     }
 
-    public function about(){
-      $data = [
-        'title' => 'About Us',
-        'description' => 'App search users'
-      ];
-
-      $this->view('pages/about', $data);
-    }
+    
   }
