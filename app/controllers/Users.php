62d1d67e46b759a043a4514760cf8f2e75cf81b4
unknown <ilijan.militar@gmail.com>
2019-12-31 08:33:21 +0100

    making register form

---
+++ app/controllers/Users.php
@@ -0,0 +1,50 @@
+<?php
+
+class Users extends Controller {
+
+    public function __construct(){
+        $this->userModel= $this->model('User');
+    }
+
+
+    public function register(){
+          // Check for POST
+      if($_SERVER['REQUEST_METHOD'] == 'POST'){
+        // Process form
+
+        // Sanitize POST data
+        $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
+        // Init data
+        $data =[
+          'name' => trim($_POST['name']),
+          'email' => trim($_POST['email']),
+          'password' => trim($_POST['password']),
+          'confirm_password' => trim($_POST['confirm_password']),
+          'name_err' => '',
+          'email_err' => '',
+          'password_err' => '',
+          'confirm_password_err' => ''
+        ];
+        
+        //FORM VALIDATION
+        
+
+      } else {
+        // Init data
+        $data =[
+          'name' => '',
+          'email' => '',
+          'password' => '',
+          'confirm_password' => '',
+          'name_err' => '',
+          'email_err' => '',
+          'password_err' => '',
+          'confirm_password_err' => ''
+        ];
+
+        // Load view just to see form before enter data
+        $this->view('users/register', $data);
+      }
+    }
+
+}
