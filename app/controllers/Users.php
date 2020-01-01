56b41dfa8b37a7377f96942f7658dba37e02a033
unknown <ilijan.militar@gmail.com>
2020-01-01 19:10:24 +0100

    destry sessiong when user logout

--- app/controllers/Users.php
+++ app/controllers/Users.php
@@ -196,6 +196,13 @@ class Users extends Controller {
       redirect('index');
     }
 
-
+// Logout & Destroy Session
+    public function logout(){
+      unset($_SESSION['user_id']);
+      unset($_SESSION['user_email']);
+      unset($_SESSION['user_name']);
+      session_destroy();
+      redirect('users/login');
+    }
 
 }
